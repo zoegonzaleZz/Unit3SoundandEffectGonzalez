1c523cea6242616244b3e00fd0a5746701061a36
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2023-03-27 09:57:09 -0700

    Working on Stop MoveLeft on gameOver
    did not finish

--- Unit3SoundandEffectGonzalez/Assets/Scripts/MoveLeft.cs
+++ Unit3SoundandEffectGonzalez/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerControllerScript.gameOver == false)
         transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
--- Unit3SoundandEffectGonzalez/Assets/Scripts/PlayerController.cs
+++ Unit3SoundandEffectGonzalez/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +29,15 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+         if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+
     }
 }
     
